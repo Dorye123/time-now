b9d88ce207bfca5e74db530b754991f82729e0f2
dorye123 <shaishamai12@gmail.com>
2024-05-04 17:28:08 +0300

    app technacally works , now need to make it aws adaptable and play with it there

---
+++ api.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/current_time')
+def get_current_time():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return {'current_time': current_time}
+
+if __name__ == '__main__':
+    app.run(debug=True)
